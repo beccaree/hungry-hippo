7f49697595d45ba72e6d04d5f32974bf1675408c
Tarotato <izhu678@aucklanduni.ac.nz>
2015-09-19 16:32:09 +1200

    Update StartFrame.java
    Added: import javax.swing.JOptionPane;

--- StartFrame.java
+++ StartFrame.java
@@ -3,6 +3,7 @@ import java.awt.EventQueue;
 
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.filechooser.FileNameExtensionFilter;
