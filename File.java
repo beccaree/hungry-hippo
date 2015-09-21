ee998eda776a82affc96edf03f7d8eb0e9d4e1e1
Becca Lee <rlee291@aucklanduni.ac.nz>
2015-09-22 11:59:09 +1200

    Create File.java
    this new file reduces duplicate code

---
+++ File.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+
+public class File {
+
+	protected static boolean isVideo(String path) {
+		//method for checking if file is a video, to prevent everything being on the same file
+		
+		String cmd = "file "+ path;
+		
+		//Determine if file chosen is a video file
+		ProcessBuilder processBuilder = new ProcessBuilder("/bin/bash", "-c", cmd);
+		Process process;
+		try {
+			process = processBuilder.start();
+			InputStream output = process.getInputStream();
+			BufferedReader stdout = new BufferedReader(new InputStreamReader(output));
+
+			String line = null;
+			while ((line = stdout.readLine()) != null) {
+				if (line.matches("(.*)video: FFMpeg MPEG-4(.*)")){
+					return true;
+				}
+			}
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}	
+		
+		return false;
+	}
+}
