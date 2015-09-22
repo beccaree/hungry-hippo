342ea131f51a49356b923d34dc2f4bf36cd4f444
Tarotato <izhu678@aucklanduni.ac.nz>
2015-09-22 20:43:28 +1200

    Added stylish comments
    Use flashy comments to comment the methods

--- File.java
+++ File.java
@@ -4,15 +4,22 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 
+/**
+ * @author Isabel Zhuang and Rebecca Lee
+ * Class contains necessary methods for completing actions in MainFrame
+ */
 public class File {
-	//class containing file checking methods
 
+	/**
+	 * Checks if the chosen file path from JFileChooser is a video (avi/mpeg4)
+	 * @param path
+	 * @return
+	 */
 	protected static boolean isVideo(String path) {
-		//method for checking if file is a video, to prevent everything being on the same file
 		
 		String cmd = "file "+ path;
 		
-		//Determine if file chosen is a video file
+		// Determines if the file path chosen is a video file
 		ProcessBuilder processBuilder = new ProcessBuilder("/bin/bash", "-c", cmd);
 		Process process;
 		try {
@@ -22,7 +29,7 @@ public class File {
 
 			String line = null;
 			while ((line = stdout.readLine()) != null) {
-				if (line.matches("(.*)video: FFMpeg MPEG-4(.*)")){
+				if (line.matches("(.*)video: FFMpeg MPEG-4(.*)")){ // Matches video format
 					return true;
 				}
 			}
@@ -33,6 +40,11 @@ public class File {
 		return false;
 	}
 	
+	/**
+	 * Checks if the chosen file path from JFileChooser is an mp3/audio file
+	 * @param path
+	 * @return
+	 */
 	protected static boolean isMp3(String path) {
 		
 		//Determine whether it is an mp3 file
@@ -56,39 +68,54 @@ public class File {
 			e1.printStackTrace();
 		}
 		
-		return false;
-		
+		return false;		
 	}
 	
+	/**
+	 * Uses BASH commands to generate an output video merged with the selected mp3 file 
+	 * @param mp3Path
+	 * @param videoPath
+	 */
 	protected static void mergeMp3(String mp3Path, String videoPath) {
 		try {
+			// Creates an mp3 file from the existing video's audio
 			String cmd = "ffmpeg -i " + videoPath + " -map 0:1 vidAudio.mp3";
 			startProcess(cmd);
 			
+			// Deletes any existing output.mp3 file
 			cmd = "rm -r output.mp3";
 			startProcess(cmd);
 			
+			// Creates an mp3 file output.mp3 that combines the video audio and selected mp3 audio for merging 
 			cmd = "ffmpeg -i " + mp3Path + " -i vidAudio.mp3 -filter_complex amix=inputs=2 output.mp3";
 			startProcess(cmd);
 			
+			// Deletes any existing output.avi file
 			cmd = "rm -r output.avi";
 			startProcess(cmd);
 			
-			Thread.sleep(200); //must sleep for previous command to finish executing
+			Thread.sleep(200); // Stops thread from continuing(sleep) so previous command can finish executing
 			
+			// Creates an output.avi file from merging combined audio (0:0) and existing video stream (1:0)
 			cmd = "ffmpeg -i output.mp3 -i " + videoPath + " -map 0:0 -map 1:0 -acodec copy -vcodec copy output.avi";
 			startProcess(cmd);
 			
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}					
 	}
 	
+	
+	/**
+	 * Starts building a process for any BASH command passed in
+	 * @param cmd
+	 * @throws IOException
+	 */
 	private static void startProcess(String cmd) throws IOException{
 		ProcessBuilder builder = new ProcessBuilder("/bin/bash", "-c", cmd);
 		builder.start();
 	}
 }
+
