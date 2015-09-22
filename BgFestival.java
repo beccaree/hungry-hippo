85bbc852f84e83d895f9dc3b7da6aa49bc275cdf
Tarotato <izhu678@aucklanduni.ac.nz>
2015-09-22 13:35:56 +1200

    Update BgFestival.java
    The array list is passed into constructor stores the festival PID

--- BgFestival.java
+++ BgFestival.java
@@ -2,43 +2,50 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 import javax.swing.SwingWorker;
 
 public class BgFestival extends SwingWorker<Void, Void> {
-		
-		protected int festID;
-		
-		private String input;
-		
-		public BgFestival(String input) {
-			this.input = input;
-		}
 
-		@Override
-		protected Void doInBackground() throws Exception {
-		
-			try {
-				//festival to speak out commentary by the user
-				String cmd = "echo \""+input+"\" | festival --tts & echo $!";
-				//echo $! from the cmd above returns the process ID of the process that has just been executed
-				
-				ProcessBuilder builder = new ProcessBuilder("/bin/bash", "-c", cmd);
-				Process process = builder.start();
-				
-				//read the output of the cmd to get the process ID for killing if necessary
-				InputStream stout = process.getInputStream();
-				BufferedReader stoutbr = new BufferedReader(new InputStreamReader(stout));
-				String line = null;
-				while((line=stoutbr.readLine()) != null) {
-					festID = Integer.parseInt(line); //store ID for cancelling later
-				}
-			} catch(Exception ex) {
-				ex.printStackTrace();
-			}
+	protected int festID;
+	private ArrayList<Integer> killPID = new ArrayList<Integer>();
+	private String input;
 
-			return null;
-		}
-	
+	public BgFestival(String input, ArrayList<Integer> killPID) {
+		this.input = input;
+		this.killPID = killPID;
 	}
 
+	@Override
+	protected Void doInBackground() throws Exception {
+
+		try {
+			// festival to speak out commentary by the user
+			String cmd = "echo \"" + input + "\" | festival --tts & echo $!";
+			// echo $! from the cmd above returns the process ID of the process
+			// that has just been executed
+
+			ProcessBuilder builder = new ProcessBuilder("/bin/bash", "-c", cmd);
+			Process process = builder.start();
+
+			// read the output of the cmd to get the process ID for killing if
+			// necessary
+			InputStream stdout = process.getInputStream();
+			BufferedReader stoutbr = new BufferedReader(new InputStreamReader(stdout));
+			String line = stdoutbr.readLine();
+			festID = Integer.parseInt(line); // store ID for cancelling later
+			System.out.println(festID);
+
+			System.out.println("Be 0:" + killPID.size());
+			killPID.add(festID);
+			System.out.println("Be 1:" + killPID.size());
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+		return null;
+	}
+
+}
