8c8867c3c17a133be211994e6194f8a26f6e83c0
Tarotato <izhu678@aucklanduni.ac.nz>
2015-09-22 14:40:49 +1200

    Update File.java
    Extracted isMp3 into a method

--- File.java
+++ File.java
@@ -5,6 +5,7 @@ import java.io.InputStreamReader;
 
 
 public class File {
+	//class containing file checking methods
 
 	protected static boolean isVideo(String path) {
 		//method for checking if file is a video, to prevent everything being on the same file
@@ -30,5 +31,32 @@ public class File {
 		}	
 		
 		return false;
+	}
+	
+	protected static boolean isMp3(String path) {
+		
+		//Determine whether it is an mp3 file
+	    String cmd = "file "+ path;
+		
+		//Determine if file chosen is a video file
+		ProcessBuilder processBuilder = new ProcessBuilder("/bin/bash", "-c", cmd);
+		Process process;
+		try {
+			process = processBuilder.start();
+			InputStream output = process.getInputStream();
+			BufferedReader stdout = new BufferedReader(new InputStreamReader(output));
+
+			String line = null;
+			while ((line = stdout.readLine()) != null) {
+				if (line.matches("(.*): Audio file(.*)")){
+					return true;
+				}
+			}
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		
+		return false;
+		
 	}
 }
