506387fc89010f1c107ef5046fdb2d4239821d79
Becca Lee <rlee291@aucklanduni.ac.nz>
2015-09-22 23:39:23 +1200

    Update saveAsDialog.java
    Added my comments and changed some formatting to look the same with other files

--- saveAsDialog.java
+++ saveAsDialog.java
@@ -62,77 +62,71 @@ public class saveAsDialog extends JDialog {
         contentPanel.add(textField);
         textField.setColumns(10);     
                 
-        {
-            JPanel buttonPane = new JPanel();
-            buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-            getContentPane().add(buttonPane, BorderLayout.SOUTH);
-            {
-                JButton okButton = new JButton("OK");
+        JPanel buttonPane = new JPanel();
+        buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        getContentPane().add(buttonPane, BorderLayout.SOUTH);
 
-                okButton.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent arg0) {
-						
-				   		if (commentary != null) {
+        JButton okButton = new JButton("OK");
+        okButton.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent arg0) {
+				if (commentary != null) {
 				   		
-							try {
-
-								textPath = System.getProperty("user.dir")+ "/.commentary.txt"; // Generate a hidden .txt file containing user commentary
-								BufferedWriter bw = new BufferedWriter(new FileWriter(textPath, false));
-								bw.write(commentary);
-								bw.close();
+        			try {
+        				textPath = System.getProperty("user.dir")+ "/.commentary.txt"; // Generate a hidden .txt file containing user commentary
+        				BufferedWriter bw = new BufferedWriter(new FileWriter(textPath, false));
+        				bw.write(commentary);
+        				bw.close();
 								
-								// Generate a sound.wav file from the saved user commentary
-								cmd = "text2wave " + textPath + " -o sound.wav";
-								startProcess(cmd);		
+        				// Generate a sound.wav file from the saved user commentary
+        				cmd = "text2wave " + textPath + " -o sound.wav";
+        				startProcess(cmd);		
 								
-								// Find out if any .mp3 file in MP3Files folder has the same name user has entered for MP3 file name
-								cmd = "find | grep -x \"./MP3Files/" + textField.getText() +".mp3\" | wc -l";
-								startProcess(cmd);
+        				// Find out if any .mp3 file in MP3Files folder has the same name user has entered for MP3 file name
+        				cmd = "find | grep -x \"./MP3Files/" + textField.getText() +".mp3\" | wc -l";
+        				startProcess(cmd);
 								
-								builder.redirectErrorStream(true);
-								InputStream stdout = process.getInputStream();
-								BufferedReader stdoutBuffered =	new BufferedReader(new InputStreamReader(stdout));
+        				builder.redirectErrorStream(true);
+        				InputStream stdout = process.getInputStream();
+        				BufferedReader stdoutBuffered =	new BufferedReader(new InputStreamReader(stdout));
 								
-								String line = stdoutBuffered.readLine();								
+        				String line = stdoutBuffered.readLine();								
 								
-								// Generate an .mp3 file if none already exists
-								if(line.equals("0")){
+        				// Generate an .mp3 file if none already exists
+        				if(line.equals("0")) {
+							cmd = "ffmpeg -i sound.wav " + "\'MP3Files/" + textField.getText() + ".mp3\'";
+        					Thread.sleep(200); //REBECCA :O ~ do we need this?-------- is it important, just wondering 
+        					startProcess(cmd);
 									
-									cmd = "ffmpeg -i sound.wav " + "\'MP3Files/" + textField.getText() + ".mp3\'";
-									Thread.sleep(200); //REBECCA :O ~ do we need this?-------- is it important, just wondering 
-									startProcess(cmd);
-									
-									JOptionPane.showMessageDialog(thisDialog, "Successfully saved "+ textField.getText() +".mp3");								
-									thisDialog.dispose();
+        					JOptionPane.showMessageDialog(thisDialog, "Successfully saved "+ textField.getText() +".mp3");								
+        					thisDialog.dispose();
 								
-								} else {
-									JOptionPane.showMessageDialog(thisDialog, "This name is taken. Please choose another.");
-								}
+        				} else {
+        					// Error dialog if name of mp3 already exists
+        					JOptionPane.showMessageDialog(thisDialog, "This name is taken. Please choose another.");
+        				}
 								
-							} catch (IOException | InterruptedException e1) {
-								e1.printStackTrace();
-							}
-						}						
-					}
-                });
-                okButton.setActionCommand("OK");
-                buttonPane.add(okButton);
+        			} catch (IOException | InterruptedException e1) {
+        				e1.printStackTrace();
+        			}
+        		}
+			}
+		});
+        okButton.setActionCommand("OK");
+        buttonPane.add(okButton);
 
-            }
-            {
-                JButton cancelButton = new JButton("Cancel");
-                cancelButton.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent arg0) {
-						thisDialog.dispose();
-					}
-				});
-                cancelButton.setActionCommand("Cancel");
-                buttonPane.add(cancelButton);
-            }
-        }
-    } 
-        
+        JButton cancelButton = new JButton("Cancel");
+        cancelButton.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent arg0) {
+        		thisDialog.dispose();
+        	}
+	});
+        cancelButton.setActionCommand("Cancel");
+        buttonPane.add(cancelButton);
+    
+    }
+    
     public void startProcess(String cmd) throws IOException{
+    	// Builds the process for the Bash command cmd
     	builder = new ProcessBuilder("/bin/bash", "-c", cmd);
 		process = builder.start();
     }
