ce132593d127807ea8c6c952e06c3cf36e085791
Becca Lee <rlee291@aucklanduni.ac.nz>
2015-09-30 13:17:10 +1300

    Final version

--- File.java
+++ File.java
@@ -1,7 +1,13 @@
+package VIDIVOX_prototype;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
 
 
 /**
@@ -10,6 +16,9 @@ import java.io.InputStreamReader;
  */
 public class File {
 
+	private static ProcessBuilder builder;
+	private static Process process;
+	
 	/**
 	 * Checks if the chosen file path from JFileChooser is a video (avi/mpeg4)
 	 * @param path
@@ -29,7 +38,7 @@ public class File {
 
 			String line = null;
 			while ((line = stdout.readLine()) != null) {
-				if (line.matches("(.*)video: FFMpeg MPEG-4(.*)")){ // Matches video format
+				if (line.matches("(.*)AVI(.*)")){ // Matches video format
 					return true;
 				}
 			}
@@ -78,28 +87,34 @@ public class File {
 	 */
 	protected static void mergeMp3(String mp3Path, String videoPath) {
 		try {
-			// Creates an mp3 file from the existing video's audio
-			String cmd = "ffmpeg -i " + videoPath + " -map 0:1 ./MP3Files/.vidAudio.mp3";
+			JOptionPane.showMessageDialog(null, "Your video will start merging as soon as you click OK, please be patient while it merges.");
+			
+			// Deletes any existing output.mp3 file
+			String cmd = "rm -r ./MP3Files/.vidAudio.mp3";
 			startProcess(cmd);
+			
+			Thread.sleep(1000); // Stops thread from continuing(sleep) so previous command can finish executing
+			
+			// Creates an mp3 file from the existing video's audio
+			cmd = "ffmpeg -i " + videoPath + " -map 0:1 ./MP3Files/.vidAudio.mp3";
+			startProcess(cmd);
+			
+			Thread.sleep(200); // Stops thread from continuing(sleep) so previous command can finish executing
 			
 			// Deletes any existing output.mp3 file
 			cmd = "rm -r ./MP3Files/.output.mp3";
 			startProcess(cmd);
 			
-			Thread.sleep(200); // Stops thread from continuing(sleep) so previous command can finish executing
+			Thread.sleep(1000);
 			
 			// Creates an mp3 file output.mp3 that combines the video audio and selected mp3 audio for merging 
 			cmd = "ffmpeg -i " + mp3Path + " -i ./MP3Files/.vidAudio.mp3 -filter_complex amix=inputs=2 ./MP3Files/.output.mp3";
 			startProcess(cmd);
 			
-			// Deletes any existing output.avi file
-			cmd = "rm -r ./VideoFiles/output.avi";
-			startProcess(cmd);
-			
-			Thread.sleep(200); // Stops thread from continuing(sleep) so previous command can finish executing
+			Thread.sleep(1000); 
 			
 			// Creates an output.avi file from merging combined audio (0:0) and existing video stream (1:0)
-			cmd = "ffmpeg -i ./MP3Files/.output.mp3 -i " + videoPath + " -map 0:0 -map 1:0 -acodec copy -vcodec copy ./VideoFiles/output.avi";
+			cmd = "ffmpeg -i ./MP3Files/.output.mp3 -i " + videoPath + " -map 0:0 -map 1:0 -acodec copy -vcodec copy ./VideoFiles/"+MainFrame.videoName+".avi";
 			startProcess(cmd);
 			
 		} catch (IOException e1) {
@@ -109,6 +124,49 @@ public class File {
 		}					
 	}
 	
+	protected static void saveAsMp3(String commentary, String name, JDialog thisDialog) {
+		if (commentary != null) {
+	   		
+			try {
+				String textPath = System.getProperty("user.dir")+ "/.commentary.txt"; // Generate a hidden .txt file containing user commentary
+				BufferedWriter bw = new BufferedWriter(new FileWriter(textPath, false));
+				bw.write(commentary);
+				bw.close();
+						
+				// Generate a hidden sound.wav file from the saved user commentary
+				String cmd = "text2wave " + textPath + " -o ./MP3Files/.sound.wav";
+				startProcess(cmd);		
+				
+				Thread.sleep(200);
+				
+				// Find out if any .mp3 file in MP3Files folder has the same name user has entered for MP3 file name
+				cmd = "find | grep -x \"./MP3Files/" + name +".mp3\" | wc -l";
+				startProcess(cmd);
+				
+				builder.redirectErrorStream(true);
+				InputStream stdout = process.getInputStream();
+				BufferedReader stdoutBuffered =	new BufferedReader(new InputStreamReader(stdout));
+						
+				String line = stdoutBuffered.readLine();								
+						
+				// Generate an .mp3 file if none already exists
+				if(line.equals("0")) {
+					cmd = "ffmpeg -i ./MP3Files/.sound.wav \'./MP3Files/" + name + ".mp3\'";
+					startProcess(cmd);
+							
+					JOptionPane.showMessageDialog(thisDialog, "Successfully saved "+ name +".mp3 to MP3Files");
+					thisDialog.dispose();
+						
+				} else {
+					// Error dialog if name of mp3 already exists
+					JOptionPane.showMessageDialog(thisDialog, "This name is taken. Please choose another.");
+				}
+						
+			} catch (IOException | InterruptedException e1) {
+				e1.printStackTrace();
+			}
+		}
+	}
 	
 	/**
 	 * Starts building a process for any BASH command passed in
@@ -116,8 +174,10 @@ public class File {
 	 * @throws IOException
 	 */
 	private static void startProcess(String cmd) throws IOException{
-		ProcessBuilder builder = new ProcessBuilder("/bin/bash", "-c", cmd);
-		builder.start();
+		builder = new ProcessBuilder("/bin/bash", "-c", cmd);
+		process = builder.start();
+		
+		
 	}
 	
 	/**
